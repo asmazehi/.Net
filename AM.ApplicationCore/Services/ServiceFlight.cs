b273cc9c6281c5858ad34e818c1f1a55940cdf70
Asma Zehi <asmazehi@gmail.com>
2025-04-25 11:25:20 +0100

    wallah la naaref win wselt

---
+++ AM.ApplicationCore/Services/ServiceFlight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServiceFlight : Service<Flight>, IServiceFlight
+    {
+        private IUnitOfWork unitOfWork;
+        public ServiceFlight(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ AM.ApplicationCore/Services/ServicePlane.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServicePlane : Service<Plane>, IServicePlane
+    {
+        IUnitOfWork unitOfWork;
+        public ServicePlane(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IList<Traveller> GetPassengers(Plane plane)
+        {
+            //IList<Traveller> passengers = new List<Traveller>();
+            //foreach (var flight in plane.ListFlights)
+            //{
+            //    foreach (var passenger in flight.ListPassengers)
+            //    {
+            //        if (passenger is Traveller traveller && !passengers.Contains(traveller))
+            //        {
+            //            passengers.Add(traveller);
+            //        }
+            //    }
+            //}
+            //return passengers;
+
+            return GetById(plane.PlaneId)
+                .ListFlights
+                .SelectMany(f => f.ListTickets)
+                .Select(t => t.MyPassenger)
+                .OfType<Traveller>()
+                .Distinct()
+                .ToList();
+        }
+    }
+}
